f092b508f6adbf6a64fffb0a43aec572e09b7309
Jon Mendizabal <jonmendi@gmail.com>
2024-02-28 14:10:47 +0100

    delete contacts component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactsComponent } from './components/contacts/contacts.component';
 
-export const routes: Routes = [
-  { path: 'contacts', component: ContactsComponent },
-  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
-];
+export const routes: Routes = [];
