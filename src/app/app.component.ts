4fdda3e39b56ae36007f2be5f300ce4787a4c870
Jon Mendizabal <jonmendi@gmail.com>
2024-02-29 00:54:58 +0100

    add filter input and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { ContactCardComponent } from './components/contact-card/contact-card.component';
 import { NewContactFormComponent } from './components/new-contact-form/new-contact-form.component';
 import { Contact } from './models/contacts';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactCardComponent, NewContactFormComponent],
+  imports: [
+    RouterOutlet,
+    ContactCardComponent,
+    NewContactFormComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -74,8 +80,11 @@ export class AppComponent {
     },
   ]);
 
+  filteredContacts = signal<Contact[]>(this.contactList());
+
   addContact(newContact: Contact) {
     this.contactList.set([newContact, ...this.contactList()]);
+    this.filteredContacts.set(this.contactList());
   }
 
   changeEditing(contact: Contact, editing: boolean) {
@@ -88,9 +97,24 @@ export class AppComponent {
         contact.id === updatedContact.id ? updatedContact : contact
       )
     );
+    this.filteredContacts.set(this.contactList());
   }
 
   deleteContact(contact: Contact) {
     this.contactList.set(this.contactList().filter((c) => c.id !== contact.id));
+    this.filteredContacts.set(this.contactList());
+  }
+
+  filter: FormControl = new FormControl('', { nonNullable: true });
+
+  filterContacts() {
+    this.filteredContacts.set(
+      this.contactList().filter((contact) =>
+        Object.values(contact).some((value) =>
+          String(value).toLowerCase().includes(this.filter.value.toLowerCase())
+        )
+      )
+    );
+    return this.filteredContacts;
   }
 }
